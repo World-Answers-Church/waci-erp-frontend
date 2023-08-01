a7096fec03b1b91b2799ba9b9a22dff295020673
Raymond Mutebi <51437128+raymond-gdhrt@users.noreply.github.com>
2023-08-02 01:17:25 +0300

    updates

--- src/app_utils/utils/EndpointsUtils.ts
+++ src/app_utils/utils/EndpointsUtils.ts
@@ -2,7 +2,7 @@ let resolveBackendEndpoint = "https://waci-erp-backend.herokuapp.com";
 if (window.location.hostname.includes("localhost") || window.location.hostname === "localhost") {
   resolveBackendEndpoint = "http://localhost:8080";
 } else {
-  resolveBackendEndpoint = "https://waci-erp-backend.herokuapp.com";
+  resolveBackendEndpoint = "https://church-erp.azurewebsites.net";
 }
 
 export const BASE_URL_ENDPOINT_PATH = resolveBackendEndpoint;
